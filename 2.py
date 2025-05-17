7f7319f4d732da85e1a947027dfbf265b7fb7f0c
Rami-kai07 <ramikai.zetolis1@gmail.com>
2025-05-17 15:47:11 -0300

    2.py

--- 2.py
+++ 2.py
@@ -1,4 +1,4 @@
 valor= int(input("Valor do Produto: "))
 divisao = int(input("Quantas vezes ser dividido: "))
 valor_da_compra= valor/divisao
-print (f"O valor de cada parcela será {valor_da_compra} reais.")
+print (f"O valor de cada parcela será {valor_da_compra:.2f} reais.")
